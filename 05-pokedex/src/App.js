6a38b8f33a2d70087e17efd69cc1d04192eccc3e
gdulik <tomas.gdula@gmail.com>
2021-02-26 11:30:11 +0100

    Updated 05-pokedex

--- 05-pokedex/src/App.js
+++ 05-pokedex/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
 	render() {
-		return <Pokedex />;
+		return <Pokegame />;
 	}
 }
 
--- 05-pokedex/src/Pokecard.js
+++ 05-pokedex/src/Pokecard.js
@@ -4,17 +4,15 @@ import './Pokecard.css';
 class Pokecard extends Component {
 	render() {
 		const { id, name, type, base_experience } = this.props;
+		const imgSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`;
 		return (
 			<div className="Pokecard">
 				<h2 className="Pokecard-title">{name}</h2>
-				<img
-					className="Pokecard-img"
-					src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`}
-				/>
-				<div className="Pokecard-text">
-					<p>Type: {type}</p>
-					<p>EXP: {base_experience}</p>
+				<div className="Pokecard-image">
+					<img src={imgSrc} alt={name} />
 				</div>
+				<div className="Pokecard-data">Type: {type}</div>
+				<div className="Pokecard-data">EXP: {base_experience}</div>
 			</div>
 		);
 	}
