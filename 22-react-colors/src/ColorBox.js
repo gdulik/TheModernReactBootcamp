3c7ce2077c79268a10107d9f4410bebd5e6d9372
gdulik <tomas.gdula@gmail.com>
2021-03-12 22:03:24 +0100

    Started react-colors-app, Added Palette, ColorBox

---
+++ 22-react-colors/src/ColorBox.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import './ColorBox.css';
+
+export default class ColorBox extends Component {
+	render() {
+		const { name, background } = this.props;
+		return (
+			<CopyToClipboard text={background}>
+				<div className="ColorBox" style={{ background }}>
+					<div className="copy-container">
+						<div className="box-content">
+							<span>{name}</span>
+						</div>
+						<button className="copy-button">Copy</button>
+					</div>
+					<span className="see-more">MORE</span>
+				</div>
+			</CopyToClipboard>
+		);
+	}
+}
---
+++ 22-react-colors/src/Palette.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
+
+class Palette extends Component {
+	render() {
+		const colorBoxes = this.props.colors.map((color) => (
+			<ColorBox background={color.color} name={color.name} />
+		));
+		return (
+			<div className="Palette">
+				{/* Navbar */}
+				<div className="Palette-colors">
+					{/* Color boxes */}
+					{colorBoxes}
+				</div>
+				{/* Footer */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
