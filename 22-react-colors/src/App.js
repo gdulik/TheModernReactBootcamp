6fc3c002580ccbdef27723200f8a69724033cb53
gdulik <tomas.gdula@gmail.com>
2021-03-13 21:47:25 +0100

    Add JSS styles, MiniPalettes, Navbar, PaletteList

--- 22-react-colors/src/App.js
+++ 22-react-colors/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+	findPalette(id) {
+		return seedColors.find((palette) => palette.id === id);
+	}
 	render() {
 		return (
-			<div>
-				<Palette {...seedColors[4]} />
-			</div>
+			<Switch>
+				<Route
+					exact
+					path="/"
+					render={() => <PaletteList palettes={seedColors} />}
+				/>
+				<Route
+					exact
+					path="/palette/:id"
+					render={(routeProps) => (
+						<Palette
+							palette={generatePalette(
+								this.findPalette(routeProps.match.params.id)
+							)}
+						/>
+					)}
+				/>
+				{/* <div>
+				<Palette palette={generatePalette(seedColors[0])} />
+			</div> */}
+			</Switch>
 		);
 	}
 }
