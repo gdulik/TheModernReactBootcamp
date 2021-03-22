7209d7324fdff1ba4550d6517474e98525aa1dd1
gdulik <tomas.gdula@gmail.com>
2021-03-22 13:31:51 +0100

    Updated styling and background

--- 22-react-colors/src/styles/PaletteListStyles.js
+++ 22-react-colors/src/styles/PaletteListStyles.js
@@ -1,17 +1,42 @@
+import sizes from './sizes';
+import bg from './bg.svg';
+
 const styles = {
+	'@global': {
+		'.fade-exit': {
+			opacity: 1
+		},
+		'.fade-exit-active': {
+			opacity: 0.01,
+			transition: 'opacity 500ms ease-out'
+		}
+	},
 	root: {
-		backgroundColor: 'blue',
+		// backgroundColor: 'blue',
 		height: '100vh',
 		display: 'flex',
 		alignItems: 'flex-start',
-		justifyContent: 'center'
+		justifyContent: 'center',
+		/* background by SVGBackgrounds.com */
+		backgroundColor: '#394bad',
+		backgroundImage: `url(${bg})`,
+		overflowY: 'scroll'
+	},
+	heading: {
+		fontSize: '2rem'
 	},
 	container: {
 		width: '50%',
 		display: 'flex',
 		alignItems: 'flex-start',
 		flexDirection: 'column',
-		flexWrap: 'wrap'
+		flexWrap: 'wrap',
+		[sizes.down('xl')]: {
+			width: '80%'
+		},
+		[sizes.down('xs')]: {
+			width: '75%'
+		}
 	},
 	nav: {
 		display: 'flex',
@@ -34,7 +59,14 @@ const styles = {
 		width: '100%',
 		display: 'grid',
 		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
+		gridGap: '2.5rem',
+		[sizes.down('md')]: {
+			gridTemplateColumns: 'repeat(2,50%)'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns: 'repeat(1,100%)',
+			gridGap: '1.4rem'
+		}
 	}
 };
 
