8908778e881b0f1e3632b7499e56ded440d7ed82
gdulik <tomas.gdula@gmail.com>
2021-03-28 23:31:41 +0200

    Updated ThemeContext and Added LanguageContext

--- 25-context/01-context-demo/src/Navbar.js
+++ 25-context/01-context-demo/src/Navbar.js
@@ -8,13 +8,19 @@ import SearchIcon from '@material-ui/icons/Search';
 import Switch from '@material-ui/core/Switch';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/NavbarStyles';
+import { ThemeContext } from './contexts/ThemeContext';
 
 class Navbar extends Component {
+	static contextType = ThemeContext;
 	render() {
+		const { isDarkMode, toggleTheme } = this.context;
 		const { classes } = this.props;
 		return (
 			<div className={classes.root}>
-				<AppBar position="static" color="primary">
+				<AppBar
+					position="static"
+					color={isDarkMode ? 'default' : 'primary'}
+				>
 					<Toolbar>
 						<IconButton
 							className={classes.menuButton}
@@ -29,7 +35,7 @@ class Navbar extends Component {
 						>
 							App Title
 						</Typography>
-						<Switch />
+						<Switch onChange={toggleTheme} />
 						<div className={classes.grow} />
 						<div className={classes.search}>
 							<div className={classes.searchIcon}>
