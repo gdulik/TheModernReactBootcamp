35c7b32e97618b82d36d9628a55db6ee66d6242f
gdulik <tomas.gdula@gmail.com>
2021-03-29 20:18:45 +0200

    Updated 01-context-demo to use hooks

--- 25-context/01-context-demo/src/PageContent.js
+++ 25-context/01-context-demo/src/PageContent.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
-export class PageContent extends Component {
-	static contextType = ThemeContext;
-	render() {
-		const { isDarkMode } = this.context;
-		const styles = {
-			backgroundColor: isDarkMode ? '#333' : '#fff',
-			height: '100vh',
-			width: '100vw'
-		};
-		return <div style={styles}>{this.props.children}</div>;
-	}
+function PageContent(props) {
+	const { isDarkMode } = useContext(ThemeContext);
+	const styles = {
+		backgroundColor: isDarkMode ? '#333' : '#fff',
+		height: '100vh',
+		width: '100vw'
+	};
+	return <div style={styles}>{props.children}</div>;
 }
+
+// class PageContent extends Component {
+// 	static contextType = ThemeContext;
+// 	render() {
+// 		const { isDarkMode } = this.context;
+// 		const styles = {
+// 			backgroundColor: isDarkMode ? '#333' : '#fff',
+// 			height: '100vh',
+// 			width: '100vw'
+// 		};
+// 		return <div style={styles}>{this.props.children}</div>;
+// 	}
+// }
 
 export default PageContent;
